8e85dae3cb3437322894a0c7f9b3794e51dc7f15
Peter McCarthy <peter.mccarthy@openmarket.com>
2020-04-19 17:19:06 +0100

    Fleshes out NumParser

--- chmodparse/parser.go
+++ chmodparse/parser.go
@@ -1,18 +1,73 @@
 package chmodparse
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Parser interface {
-	Parse(s string) string
+	Parse(arg string) (string, error)
 }
 
 type NumParser struct {
+	specialMode rune
+	mode        string
 }
 type CharParser struct {
 }
 
-func (p NumParser) Parse(s string) string {
-	return "IMPLEMENT NUMPARSER"
+func NewNumParser(arg string) NumParser {
+	p := NumParser{}
+	if len(arg) == 4 {
+		p.specialMode = arg[0]
+		p.mode = strings.arg[1:3]
+	} else {
+		p.mode = arg
+	}
+	return p
 }
 
-func (p CharParser) Parse(s string) string {
-	return "IMPLEMENT CHARPARSER"
+var modes = map[string]string{
+	"0": "---",
+	"1": "--x",
+	"2": "-w-",
+	"3": "-wx",
+	"4": "r--",
+	"5": "r-x",
+	"6": "rw-",
+	"7": "rwx",
+}
+
+func (p NumParser) Parse(arg string) (string, error) {
+	builder := strings.Builder{}
+
+	if p.specialMode != 0 {
+		builder.WriteString("Special Modes:\n")
+		//TODO implement special modes
+	}
+
+	builder.WriteString(p.mode)
+
+	return builder.String(), nil
+}
+
+func (p CharParser) Parse(arg string) (string, error) {
+	a, err := reverseMap(modes, arg)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Given %s, we get %s", arg, a)
+
+	return "IMPLEMENT CHARPARSER", nil
+}
+
+// Given rwx as val, will return 7 as k
+func reverseMap(m map[string]string, val string) (string, error) {
+	for k, v := range m {
+		if val == v {
+			return k, nil
+		}
+	}
+	return "", errors.New("Invalid argument")
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/PMcca/phmod
 
 go 1.14
 
-require (
-	github.com/urfave/cli v1.22.4
-)
+require github.com/urfave/cli v1.22.4
